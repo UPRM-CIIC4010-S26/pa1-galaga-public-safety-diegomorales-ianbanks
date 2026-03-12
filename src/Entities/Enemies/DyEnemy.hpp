dc98e6448bfcd0aa0b414502af43e818f1bae27b
banks-masso-ian <ian.banks@upr.edu>
2026-03-11 22:51:12 -0400

    Made it so DyEnemies have alternating textures

--- src/Entities/Enemies/DyEnemy.hpp
+++ src/Entities/Enemies/DyEnemy.hpp
@@ -1,17 +1,22 @@
 #pragma once
 #include "Enemy.hpp"
+static int nextTexture = 0;
 
 class DyEnemy : public Enemy {
     private: 
         float angle = 135;
         float aimAngle = 225;
         bool loop = false;
+        int randomNumber;
+
 
     public:
         int pointvalue = 100;
         DyEnemy(float x, float y) : Enemy(x, y) { 
             this->cooldown = GetRandomValue(90, 300);
-            this->health = 1; 
+            this->health = 1;
+            this->randomNumber = nextTexture;
+            nextTexture = 1 - nextTexture;
         }
 
         void draw() override;
